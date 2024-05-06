0a4001ece75f3e5c7d84b802c680920de8cd0187
Hugo Fernando Canizales Andrade <ca18052@ues.edu.sv>
2024-05-05 20:38:40 -0600

    com.iis.ca18052

---
+++ demo-ca18052/src/main/java/com/iis/ca18052/App.java
@@ -0,0 +1,19 @@
+package com.iis.ca18052;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello () {
+		return "Hello World";
+	}
+	
+    public static void main( String[] args )
+    {
+        App hw = new App ();
+    	System.out.println(hw.Hello() );
+        
+    }
+}
